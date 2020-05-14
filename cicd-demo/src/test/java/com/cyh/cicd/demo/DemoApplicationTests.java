ec2cad411f41a4ae34c3c63c6492cb72203344a4
v_yhuancai <randycai@21kunpeng.com>
2020-05-14 17:28:29 +0800

    modify some code

--- cicd-demo/src/test/java/com/cyh/cicd/demo/DemoApplicationTests.java
+++ cicd-demo/src/test/java/com/cyh/cicd/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package com.cyh.cicd.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
-@SpringBootTest
-class DemoApplicationTests {
+@RunWith(MockitoJUnitRunner.class)
+public class DemoApplicationTests {
+
+	@BeforeEach
+	public void setUp() {
+
+	}
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
