95eb935c812072073407dce4d1dec06f15d843c8
v_yhuancai <randycai@21kunpeng.com>
2020-06-09 08:50:47 +0800

    test jenkins cicd

---
+++ cicd-demo/src/main/java/com/cyh/cicd/demo/api/TestController.java
@@ -0,0 +1,18 @@
+package com.cyh.cicd.demo.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+@RestController
+public class TestController {
+
+    @GetMapping(value = "hello")
+    public String hello() {
+        return "hello " + DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now());
+    }
+}
